96e155fbb5343430845d2c77ee115f5a09ed9ed2
Reginaakhmathanova <rrakhmathanova@gmail.com>
2025-05-03 21:17:04 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,3 +42,33 @@ function changeForm (){
 overlay.addEventListener('click', hideForm);
 closeBtn.addEventListener('click', hideForm);
 mobileMenuBtn.addEventListener('click', changeForm);
+
+/*
+Алгоритм смены фона:
+1. Создаем массив с путями к фоновым изображениям.
+2. Заводим переменную-счётчик для отслеживания текущего индекса.
+3. Получаем элементы: кнопку и блок .info-big.
+4. При каждом клике по кнопке:
+   - устанавливаем следующее изображение из массива в качестве фона;
+   - увеличиваем счётчик;
+   - если счётчик превышает количество изображений, обнуляем его.
+*/
+
+const backgroundImages = [
+    '../images/bg1.png',
+    '../images/bg2.png',
+    '../images/emoji_bg.webp',
+];
+
+let currentIndex = 0;
+
+const button = document.getElementById('changeBackgroundBtn');
+const section = document.querySelector('.info-big');
+
+button.addEventListener('click', () => {
+    section.style.backgroundImage = `url(${backgroundImages[currentIndex]})`;
+    section.style.backgroundSize = 'cover';
+    section.style.backgroundPosition = 'center';
+
+    currentIndex = (currentIndex + 1) % backgroundImages.length;
+});
